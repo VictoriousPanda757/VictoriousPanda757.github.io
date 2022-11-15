567bc8dd456fb41dc59a7a96bdac0ad513d047d5
Anonymous <>
2022-11-15 16:23:24 -0500

    Add age to user card

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -70,6 +70,8 @@ export default function User({ user, favoriteUsers, setFavoriteUsers }: Props) {
         </p>
         <p>
           <strong>Birthday:</strong> {formatDate(user.dob.date)}
+          <br />
+          <strong>Age:</strong> {user.dob.age}
         </p>
         <p>
           <strong>Location:</strong> {user.location.street.number}{" "}
